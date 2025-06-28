5fb06b0db6670227d419dd23897059a0816adfe9
NoobCoder143 <avinav.chowdhury7@gmail.com>
2025-06-28 19:32:27 +0530

    Displaying uploaded files using fs and enabling download of those files using res.download

---
+++ script.js
@@ -0,0 +1,19 @@
+fetch("/files").then(res=>res.json()).then(data=>{
+    const tablebody=document.querySelector('#fileTable tbody');
+      console.log(data);
+   if(Array.isArray(data)){ data.forEach(item => {
+        const row=document.createElement("tr")
+       const sizeInKB = (item.size / 1024).toFixed(2) + " KB";//convert bytes into kilobytes,toFixed(2) rounds result upto 2 decimal places
+      const uploaded = new Date(item.uploadedAt).toLocaleString();//converts a timestamp into a human-readable date and time string based on the user's local settings.
+        row.innerHTML=`
+        <td>${item.name}</td> 
+        <td>${sizeInKB}</td>
+        <td>${uploaded}</td>
+       <td><form action="/download" method="get">
+        <input type="hidden" name="filename" value="${item.name}">
+        <button type="submit">Download</button>
+        </form></td>`;
+        tablebody.appendChild(row);
+    });
+    }
+})
--- server.js
+++ server.js
@@ -1,8 +1,12 @@
 const express = require('express');
 const multer  = require('multer')
- 
+const fs=require('fs')
+const path=require('path')
 const app=express();
 const port=3000;
+app.use(express.static(__dirname)); // This tells Express: "Serve any file (like script.js, index.html) from the current folder."
+
+const uploaddir=path.join(__dirname,"upload");//this is the path to the upload folder
 
 const storage = multer.diskStorage({
     //cb - callback
@@ -12,6 +16,27 @@ const storage = multer.diskStorage({
   filename: function (req, file, cb) {
     cb(null,`${Date.now()}+${file.originalname}`) // cb(error,file name)
   }
+})
+//this function reads  and gets the metadata  for each file in the upload folder using fs.stat(file system)
+const getfileMetadata=()=>{
+  //this reads all the files in upload folder one by one
+  const files=fs.readdirSync(uploaddir);
+  //this function then returns he metadata for each file(name,size,uploadedAt)
+ const fileData= files.map((file)=>{
+   const filePath=path.join(uploaddir,file)
+   const stats = fs.statSync(filePath)
+   return {
+    name:file,
+    size: stats.size,
+    uploadedAt:stats.birthtime
+   }
+  })
+  return fileData
+}
+
+app.get("/files",(req,res)=>{
+  const fileMetadata=getfileMetadata();
+  res.json(fileMetadata)
 })
  
 const upload = multer({ storage: storage })
@@ -23,4 +48,16 @@ app.get("/",(req,res)=>{
     //'/index.html' adds the filename to that path.
 res.sendFile(__dirname + '/index.html');
 })
+
+app.get("/download",(req,res)=>{
+ const fileName=req.query.filename;
+  res.download(`${__dirname}/upload/${fileName}`,(err)=>{
+    if(err){
+      console.log("file not found")
+      res.send("file not found").status(404)
+    }
+  })
+
+})
+
 app.listen(port,()=>{console.log("Listening at http://localhost:3000")})
